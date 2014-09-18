d3dfec687284a61b5878242af386520838e891c7
huili1725 <Jawan@ACA8016F.ipt.aol.com>
2014-09-18 14:29:13 +0800

    首页更新

--- src/com/free/assist/web/homepage/HomePageAction.java
+++ src/com/free/assist/web/homepage/HomePageAction.java
@@ -1,16 +1,36 @@
 package com.free.assist.web.homepage;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
 import org.directwebremoting.WebContext;
 import org.directwebremoting.WebContextFactory;
 import org.springframework.stereotype.Controller;
 
+import com.free.assist.domain.BusLinePipeKnow;
+import com.free.assist.domain.BusLinePipeKnowExample;
+import com.free.assist.domain.BusLinePipeKnowledge;
+import com.free.assist.domain.BusLinePipeKnowledgeExample;
+import com.free.assist.domain.BusLinePipeRule;
+import com.free.assist.domain.BusLinePipeRuleExample;
+import com.free.assist.domain.BusReleaseNews;
+import com.free.assist.domain.BusReleaseNewsExample;
 import com.free.assist.domain.BusReleaseTrends;
 import com.free.assist.domain.BusReleaseTrendsExample;
+import com.free.assist.domain.SysModule;
+import com.free.assist.domain.SysModuleExample;
+import com.free.assist.domain.SysRoleModuleExample;
+import com.free.assist.domain.SysRoleModuleKey;
+import com.free.assist.domain.SysUser;
+import com.free.assist.domain.SysUserRoleExample;
+import com.free.assist.domain.SysUserRoleKey;
 import com.free.assist.service.business.DynamicOperateService;
 import com.free.assist.web.BaseAction;
 
@@ -30,4 +50,50 @@ public class HomePageAction extends BaseAction {
 		request.setAttribute("trendsList", trendsList);
 		return wctx.forwardToString("/jsp/dynamic/trendsList.jsp");
 	}
+	
+	/**
+	 * 获取首页展示数据
+	 * @param form 
+	 * @return
+	 * @throws Exception
+	 */
+	public ActionForward getData(ActionMapping mapping,ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+        // 取得施工动态
+        BusReleaseTrendsExample ex = new BusReleaseTrendsExample();
+        ex.setOrderByClause(" create_time desc");
+        List<BusReleaseTrends> trendsList = dynamicOperateService.selectByExample(ex);
+        request.setAttribute("trendsList", trendsList);
+        
+
+        // 取得管道常识
+        BusLinePipeKnowledgeExample exKnowledge = new BusLinePipeKnowledgeExample();;
+        exKnowledge.setOrderByClause(" create_time desc");
+        List<BusLinePipeKnowledge> knowledgeList = dynamicOperateService.selectByExample(exKnowledge);
+        request.setAttribute("knowledgeList", knowledgeList);
+
+
+        // 取得管线识别
+        BusLinePipeKnowExample exKnow = new BusLinePipeKnowExample();
+        exKnow.setOrderByClause(" create_time desc");
+        List<BusLinePipeKnow> knowList = dynamicOperateService.selectByExample(exKnow);
+        request.setAttribute("knowList", knowList);
+        
+
+        // 取得管线法规
+        BusLinePipeRuleExample exRule = new BusLinePipeRuleExample();
+        exRule.setOrderByClause(" create_time desc");
+        List<BusLinePipeRule> ruleList = dynamicOperateService.selectByExample(exRule);
+        request.setAttribute("ruleList", ruleList);
+
+
+        // 取得新闻动态
+        BusReleaseNewsExample exNews = new BusReleaseNewsExample();
+        exNews.setOrderByClause(" create_time desc");
+        List<BusReleaseNews> newsList = dynamicOperateService.selectByExample(exNews);
+        request.setAttribute("newsList", newsList);
+        
+		return new ActionForward("/jsp/common/homeData.jsp");
+	}
+
 }
